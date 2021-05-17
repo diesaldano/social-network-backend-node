3d01984f61aff4b068df73b0ff774c4bdbac15a5
Diego Saldaño <diefesal@gmail.com>
2021-05-17 00:20:11 -0300

    add changes in config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   remoteDB: process.env.REMOTE_DB || false,
   api: {
-    port: process.env.API_PORT || 3004
+    port: process.env.API_PORT || 3000
   },
   post: {
-    port: process.env.API_PORT || 3005
+    port: process.env.API_PORT || 3002
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'Secret!'
@@ -17,12 +17,16 @@ module.exports = {
   },
   mysqlService: {
     host: process.env.MYSQL_SRV_HOST || 'localhost',
-    port: process.env.MYSQL_SRV_PORT || 3006
+    port: process.env.MYSQL_SRV_PORT || 3001
   },
   cacheService: {
+    host: process.env.MYSQL_SRV_HOST || 'localhost',
+    port: process.env.MYSQL_SRV_PORT || 3003
+  },
+  redis: {
     host: process.env.REDIS_HOST || 'redis-17022.c15.us-east-1-2.ec2.cloud.redislabs.com',
     port: process.env.REDIS_PORT || 17022,
-    password: process.env.REDIS_PASSWORD || '3k2MDpj8ftd5ktZU8c52bGKgTNl6VfaD',
+    password: process.env.REDIS_PASS || '3k2MDpj8ftd5ktZU8c52bGKgTNl6VfaD',
   }
 
 }
